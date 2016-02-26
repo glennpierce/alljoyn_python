5cd78c452f0d2aa99badc77853af49d544ef0594
Glenn Pierce <glennpierce@gmail.com>
2016-02-26 09:09:55 +0000

    Tidy ups

--- tools/clang2json.py
+++ tools/clang2json.py
@@ -12,10 +12,11 @@ def get_ts(source_path):
 
 
 doc = None
-typedefs = defaultdict(list)
+typedefs = []
 enums = defaultdict(list)
 structs = defaultdict(list)
 functions = defaultdict(list)
+function_pointers = defaultdict(list)
 
 
 def get_parent_with_type(node, type):
@@ -23,20 +24,24 @@ def get_parent_with_type(node, type):
         node = node.parent_node
     return node
 
+def get_return_type(node):
+    # Can't find a better way
+    result = node.type.get_result().spelling
+    result_name = node.get_tokens().next().spelling
+    
+    if result_name.lower() == "qstatus":
+        return result_name
+        
+    return result
+    
+
 def print_node(node, parent_node):
     node.parent_node = parent_node
     
     #underlying_typedef_type
+    #CursorKind.TYPEDEF_DECL
+    #CursorKind.TYPE_REF   should have used this for function poiters. oh well
     
-    #if node.kind == clang.cindex.CursorKind.INTEGER_LITERAL:
-        ## Could maybe use enum_value and enum_type. I did not know about it when I wrote below
-        #typedef_node = get_parent_with_type(node, clang.cindex.CursorKind.TYPEDEF_DECL)
-        #if typedef_node:
-            #container = enums[typedef_node.spelling]
-            #container.append({'enum_name' : node.parent_node.spelling,
-                              #'enum_type' : node.semantic_parent.enum_type.spelling,
-                              #'enum_value' : node.enum_value})
-
     if node.kind == clang.cindex.CursorKind.ENUM_CONSTANT_DECL:
         # Could maybe use enum_value and enum_type. I did not know about it when I wrote below
         typedef_node = get_parent_with_type(node, clang.cindex.CursorKind.TYPEDEF_DECL)
@@ -71,21 +76,22 @@ def print_node(node, parent_node):
             variadic = node.type.is_function_variadic()
         
         #print [n.spelling for n in node.get_tokens()]
-        container.append({'displayname':node.displayname, 'return_type': node.get_tokens().next().spelling,
+        container.append({'displayname':node.displayname, 'return_type': get_return_type(node),
                           'params':params, 'is_variadic':variadic})
         
     elif node.kind == clang.cindex.CursorKind.PARM_DECL:
         typedef_node = get_parent_with_type(node, clang.cindex.CursorKind.TYPEDEF_DECL)
        
         if typedef_node:
-            container = typedefs[typedef_node.spelling]
+            container = function_pointers[typedef_node.spelling]
             container.append((node.type.spelling, str(node.type.kind).split(".")[1], node.spelling))
+    elif node.kind == clang.cindex.CursorKind.TYPEDEF_DECL: 
+        typedefs.append((node.underlying_typedef_type.spelling, node.spelling))
     else:
-        #print node.kind
-        #print node.spelling, node.location
+        print node.kind
+        print node.spelling, node.location
         pass
 
-   
     [print_node(n, node) for n in node.get_children()]
 
 
@@ -114,6 +120,7 @@ if __name__ == "__main__":
             'doc': os.path.split(ts.spelling)[1],
             'enums': dict(enums),
             'structs': dict(structs),
+            'function_pointers': dict(function_pointers),
             'typedefs': dict(typedefs),
             'functions': dict(functions)
           }, indent=4)
